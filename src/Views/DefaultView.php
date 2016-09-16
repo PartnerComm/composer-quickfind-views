410fdcce4c35bdb44eb7bbb0a0e620e3a9827f24
Chadwick Cole <ccole@partnercomm.net>
2016-09-15 22:39:55 -0400

    update setView method for selecting correct template

--- src/Views/DefaultView.php
+++ src/Views/DefaultView.php
@@ -7,13 +7,29 @@ class DefaultView extends ViewOne {
     public function __construct(array $data, $title='', $atd = FALSE)
     {
         parent::__construct($data, $title, $atd);
+    }
 
-        if($data['isGroup']) {
-            $this->setTemplate('default-group');
+    protected function setTemplate($template)
+    {
+        if(file_exists(__DIR__.'/Templates/'.$template.'.php')) {
+            $this->template = __DIR__.'/Templates/'.$template.'.php';
+        }
 
-            if(!empty($data['table']['rows'])) {
-                $this->setTemplate('default-table');
+        if($this->altTemplateDirectory && file_exists($this->altTemplateDirectory.'/'.$template.'.php')) {
+            $this->template = $this->altTemplateDirectory.'/'.$template.'.php';
+        }
+
+        if(empty($this->template)) {
+            $this->template = __DIR__.'/Templates/default.php';
+
+            if($this->data['isGroup']) {
+                $this->template = __DIR__.'/Templates/default-group.php';
+
+                if(!empty($this->data['table']['rows'])) {
+                    $this->template = __DIR__.'/Templates/default-table.php';
+                }
             }
+
         }
     }
 }
--- src/Views/Templates/contact-group.php
+++ src/Views/Templates/contact-group.php
@@ -1,4 +1,3 @@
-<?php $contact = new \PCommContacts(); ?>
 <div class="wrap_contact_group">
 <div class="container"><h2>Contacts for <?php //echo $page_title; ?></h2></div>
     <div class="container"><h2>Contacts</h2></div>
@@ -8,26 +7,7 @@
             <div class="contact_tile <?php echo $post->getStructure()->getGroupView(); ?>">
                 <article class="article_content">
                     <h3> <?php echo $post->getPostTitle(); ?></h3>
-                    <?php $pc_administrator = $post->getCustom('pc_administrator'); ?>
-                    <?php if(!empty($pc_administrator[0])): ?>
-                        <div class="administrators"><?php echo $contact->output_administrator(unserialize($pc_administrator[0])); ?></div>
-                    <?php endif; ?>
-                    <?php $pc_phone_number = $post->getCustom('pc_phone_number'); ?>
-                    <?php if(!empty($pc_phone_number[0])): ?>
-                        <div class="contact_phones"><?php echo $contact->output_phone_numbers(unserialize($pc_phone_number[0])); ?></div>
-                    <?php endif; ?>
 
-                    <?php $pc_url = $post->getCustom('pc_url'); ?>
-                    <?php if(!empty($pc_url[0])): ?>
-                        <div class="contact_urls">
-                            <div class="handle_breaks"> <?php echo $contact->output_urls(unserialize($pc_url[0]))); ?></div>
-                        </div>
-                    <?php endif; ?>
-
-                    <?php $pc_mailing_address = $post->getCustom('pc_mailing_address'); ?>
-                    <?php if(!empty($pc_mailing_address[0])): ?>
-                        <div class="contact_addresses"><?php echo $contact->output_mailing_address(unserialize($pc_mailing_address[0])); ?> </div>
-                    <?php endif; ?>
                 </article>
             </div>
         <?php endforeach; ?>
